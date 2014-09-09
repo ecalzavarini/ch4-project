9bdadcb9121939d6e97f59b70f7df8df08234f87
Enrico Calzavarini <ecalzavarini@gmail.com>
2014-09-09 15:23:39 +0200

    Kalyan changes for jet and inlet/outlet in define.h

--- src/define.h
+++ src/define.h
@@ -20,14 +20,14 @@
 //#define GRID_REFINED_BULK
 //#define GRID_REFINED_CHEBYSHEV
 //#define GRID_REFINED_SINH
-//#define GRID_REFINED_X
+//#define GRID_REFINED_X  /* activate direction to be refined */
 //#define GRID_REFINED_Y
 //#define GRID_REFINED_Z
 
 //#define GRID_IRREGULAR_RANDOM
 
-/* Our default choice is D3Q19 and is defined below */
-//#define GRID_POP_D2Q9
+/* Our default choice is D3Q19 and is defined at the end of this file */
+//#define GRID_POP_D2Q9 
 //#define GRID_POP_D3Q15
 //#define GRID_POP_D3Q19
 //#define GRID_POP_D3Q27
@@ -36,7 +36,7 @@
 #define LB
 
 /* FLUID */
-#define LB_FLUID
+#define LB_FLUID 
 //#define LB_FLUID_INITIAL_KOLMOGOROV 
 //#define LB_FLUID_INITIAL_POISEUILLE
 //#define LB_FLUID_INITIAL_POISEUILLE_HALF
@@ -44,8 +44,8 @@
 //#define LB_FLUID_INITIAL_VORTICES 
 //#define LB_FLUID_INITIAL_PERTURBATION
 //#define LB_FLUID_INITIAL_LANDSCAPE
-#define LB_INITIAL_BAROMETRIC
-#define LB_FLUID_FORCING
+//#define LB_INITIAL_BAROMETRIC
+#define LB_FLUID_FORCING  /* activate force on the fluid */
 //#define LB_FLUID_FORCING_CHANNEL
 //#define LB_FLUID_FORCING_POISEUILLE
 //#define LB_FLUID_FORCING_CONSTANT_POWER
@@ -54,9 +54,9 @@
 //#define LB_FLUID_FORCING_PENALIZATION
 //#define LB_FLUID_FORCING_DIRECT
 //#define LB_FLUID_FORCING_CELLULAR
-#define LB_FLUID_FORCING_HIT
+//#define LB_FLUID_FORCING_HIT
 
-/* LANDSCAPE DEFINITION IN THE DOMAIN */
+/* Landscape definitions in the domain */
 //#define LB_FLUID_FORCING_LANDSCAPE
 //#define CYLINDER
 //#define CUBE
@@ -64,13 +64,15 @@
 
 #define LB_FLUID_BC
 #define LB_FLUID_BC_Y  /* default fluid bc is no-slip */
-//#define LB_FLUID_BC_YP_SLIP                                           
-//#define LB_FLUID_BC_YM_SLIP                         
+//#define LB_FLUID_BC_YM_SLIP                                           
+//#define LB_FLUID_BC_YP_SLIP  
+//#define LB_FLUID_BC_YM_JET                       
 //#define LB_FLUID_BC_YP_OUTLET
 //#define LB_FLUID_BC_X                                                     
-//#define LB_FLUID_BC_XP_SLIP                                             
-//#define LB_FLUID_BC_XM_SLIP                                               
+//#define LB_FLUID_BC_XM_SLIP                                             
+//#define LB_FLUID_BC_XP_SLIP                                               
 //#define LB_FLUID_BC_XM_INLET                                         
+//#define LB_FLUID_BC_XM_OUTLET 
 //#define LB_FLUID_BC_XP_OUTLET                                               
 //#define LB_FLUID_BC_Z  
 
@@ -106,28 +108,28 @@ Activate either METHOD_FINITE_VOLUME or METHOD_STREAMING */
 /* TEMPERATURE */
 #define LB_TEMPERATURE
 //#define LB_TEMPERATURE_FLUCTUATION
-#define LB_TEMPERATURE_INITIAL_LINEAR
-#define LB_TEMPERATURE_INITIAL_ADD_PERTURBATION
-//#define LB_TEMPERATURE_INITIAL_CONSTANT
+//#define LB_TEMPERATURE_INITIAL_LINEAR
+//#define LB_TEMPERATURE_INITIAL_ADD_PERTURBATION
+#define LB_TEMPERATURE_INITIAL_CONSTANT
 //#define LB_TEMPERATURE_INITIAL_CONSTANT_MEAN
 //#define LB_TEMPERATURE_INITIAL_CONSTANT_BOT
-//#define LB_TEMPERATURE_INITIAL_CONSTANT_TOP
+#define LB_TEMPERATURE_INITIAL_CONSTANT_TOP
 //#define LB_TEMPERATURE_INITIAL_SPOT
 //#define LB_TEMPERATURE_INITIAL_BL
-#define LB_TEMPERATURE_BUOYANCY
+//#define LB_TEMPERATURE_BUOYANCY
 #define LB_TEMPERATURE_BC
 #define LB_TEMPERATURE_BC_Y
 //#define LB_TEMPERATURE_BC_Y_VARIABLE
 //#define LB_TEMPERATURE_BC_X
 //#define LB_TEMPERATURE_BC_X_NOFLUX
 //#define LB_TEMPERATURE_BC_KEEP_WITHIN
-//#define LB_TEMPERATURE_FORCING
+#define LB_TEMPERATURE_FORCING
 //#define LB_TEMPERATURE_FORCING_BULK 
 //#define LB_TEMPERATURE_FORCING_BULK_VARIABLE
 //#define LB_TEMPERATURE_FORCING_RADIATION 
 //#define LB_TEMPERATURE_FORCING_SOURCE
 //#define LB_TEMPERATURE_FORCING_PROFILE
-//#define LB_TEMPERATURE_FORCING_REACTION
+#define LB_TEMPERATURE_FORCING_REACTION
 //#define LB_TEMPERATURE_MELTING
 //#define LB_TEMPERATURE_FORCING_DIRECT
 
