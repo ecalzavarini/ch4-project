65e0998459931ae09471c870bbcb119ee20e3d57
Enrico Calzavarini <ecalzavarini@gmail.com>
2015-03-17 00:05:35 +0100

    init conditions

--- src/initial_conditions.c
+++ src/initial_conditions.c
@@ -283,18 +283,21 @@ void initial_conditions(int restart)
    /* communicate borders for populations */
    sendrecv_borders_pop(g);
 
+
+ #ifdef LB_TEMPERATURE_FORCING
    /* Melting fields */
- #ifdef LB_TEMPERATURE_MELTING
+  #ifdef LB_TEMPERATURE_MELTING
   /* 1 is fluid , 0 is solid */
    for(k=BRD;k<LNZ+BRD;k++)
     for(j=BRD;j<LNY+BRD;j++)
       for(i=BRD;i<LNX+BRD;i++){ 
-  #ifdef LB_TEMPERATURE_MELTING_INITIAL_LIQUID
+   #ifdef LB_TEMPERATURE_MELTING_INITIAL_LIQUID
       liquid_frac[IDX(i, j, k)]=liquid_frac_old[IDX(i, j, k)]=1.0;
-  #else
+   #else
       liquid_frac[IDX(i, j, k)]=liquid_frac_old[IDX(i, j, k)]=0.0;
-  #endif
+   #endif
       }  
+  #endif
  #endif
 
 #endif /* end of LB_TEMPERATURE */
