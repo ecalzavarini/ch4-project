8e55c20212f568911558408e2cab291e374a61cc
Enrico Calzavarini <ecalzavarini@gmail.com>
2015-04-10 23:43:20 +0200

    change on python utils

--- src/python/h5_particle.py
+++ src/python/h5_particle.py
@@ -8,21 +8,24 @@ import os
 fname = sys.argv[1]
 #f = h5py.File('/Users/enrico/here/HIT/RUN/particle_5000.h5','r')
 f = h5py.File( fname,'r') 
+families = 15
+feature = sys.argv[2]
 
 name=np.array( f['lagrange']['name'])
-gyro=np.array(f['lagrange']['gyrotaxis_velocity'])
+#gyro=np.array(f['lagrange']['gyrotaxis_velocity'])
+gyro=np.array(f['lagrange'][feature]) 
 x=np.array( f['lagrange']['x'])
 y=np.array( f['lagrange']['y'])
 z=np.array( f['lagrange']['z'])
 
 npart = gyro.shape[0]
-which_part = float(sys.argv[2])
+which_part = float(sys.argv[3])
 
 
 f = open('myfile','w')
 
 for i in xrange(0,npart):
-     if name[i]%13 == which_part :
+     if name[i]%families == which_part :
           print(name[i],gyro[i],x[i],y[i],z[i],file=f)
 
 f.close()
