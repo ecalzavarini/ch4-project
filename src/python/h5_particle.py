141ce99b663ebd1c7fab32e29b66cc8890150597
Enrico Calzavarini <ecalzavarini@gmail.com>
2015-07-20 17:45:09 +0800

    extract particle trajectories

--- src/python/h5_particle.py
+++ src/python/h5_particle.py
@@ -1,34 +1,49 @@
-#!/usr/local/bin/python
+#!/usr/local/bin/python                                                                                        
 from __future__ import print_function
 import h5py
 import numpy as np
 import sys
 import os
 
-fname = sys.argv[1]
-#f = h5py.File('/Users/enrico/here/HIT/RUN/particle_5000.h5','r')
-f = h5py.File( fname,'r') 
-families = 15
-feature = sys.argv[2]
-
-name=np.array( f['lagrange']['name'])
-#gyro=np.array(f['lagrange']['gyrotaxis_velocity'])
-gyro=np.array(f['lagrange'][feature]) 
-x=np.array( f['lagrange']['x'])
-y=np.array( f['lagrange']['y'])
-z=np.array( f['lagrange']['z'])
-
-npart = gyro.shape[0]
-which_part = float(sys.argv[3])
+import re
+numbers = re.compile(r'(\d+)')
+def numericalSort(value):
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
 
 
-f = open('myfile','w')
+dirname = sys.argv[1]
+filenames = os.listdir(dirname)
+filenames = sorted(filenames, key=numericalSort)
 
-for i in xrange(0,npart):
-     if name[i]%families == which_part :
-          print(name[i],gyro[i],x[i],y[i],z[i],file=f)
+for file in filenames:
+    if 'particle' in file:
+        if 'h5' in file:
+            fpath = dirname+"/"+file
+            #print(fpath)
+            fin = h5py.File( fpath,'r')
 
-f.close()
+            name=np.array( fin['lagrange']['name'])
+            x=np.array( fin['lagrange']['x'])
+            y=np.array( fin['lagrange']['y'])
+            z=np.array( fin['lagrange']['z'])
+            vx=np.array( fin['lagrange']['vx'])
+            vy=np.array( fin['lagrange']['vy'])
+            vz=np.array( fin['lagrange']['vz'])
+
+
+            npart = name.shape[0]
+            which_part = float(sys.argv[2])
+
+
+            fout = open('myfile','a')
+
+            for i in xrange(0,npart):
+               if name[i] == which_part :
+                    print(name[i],x[i],y[i],z[i],vx[i],vy[i],vz[i],file=fout)
+
+            fout.close()
 
 
 os.system("cat myfile") 
