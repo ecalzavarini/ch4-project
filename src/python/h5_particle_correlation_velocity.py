9be33c8bf0c707a45d45826e800b98c6ad80760f
Enrico Calzavarini <ecalzavarini@gmail.com>
2016-05-22 20:01:58 +0200

    particle velocity correlation

--- src/python/h5_particle_correlation_velocity.py
+++ src/python/h5_particle_correlation_velocity.py
@@ -12,7 +12,6 @@ def numericalSort(value):
     parts[1::2] = map(int, parts[1::2])
     return parts
 
-
 dirname = sys.argv[1]
 dirnumstart = sys.argv[2]
 dirnumend = sys.argv[3]
@@ -36,14 +35,17 @@ def acf(series):
     acf_coeffs = map(r, x)
     return acf_coeffs
 
-# create array
+#create arrays
 acceleration_x = np.array([])
 acceleration_y = np.array([])
 acceleration_z = np.array([])
 
+#number of particle types 
 ntype = int(sys.argv[5])
+# total number of particles
+totpart = int(sys.argv[6])
 n=0.0
-for j in xrange(0,22000,ntype):
+for j in xrange(0,totpart,ntype):
     # loop on all files
     for i in xrange(int(dirnumstart),int(dirnumend)+1):
         for file in filenames:
@@ -56,14 +58,14 @@ for j in xrange(0,22000,ntype):
 # read the group
                     group = fin['lagrange']
                     labels = group.keys()
+
 #read the particle data
                     name=np.array( group['name'])
                     ax=np.array( group['vx'])
                     ay=np.array( group['vy'])
                     az=np.array( group['vz'])
 
-#            npart = name.shape[0]
-#            print(ax[0])
+#append the data of a given particle
                     which_part = int(sys.argv[4])+j           
 #                    print(which_part)
                     acceleration_x = np.append(acceleration_x,ax[which_part])
@@ -71,14 +73,20 @@ for j in xrange(0,22000,ntype):
                     acceleration_z = np.append(acceleration_z,az[which_part])
                     fin.close()
 
- 
-#print(acceleration)
+#compute spectra 
     tmp_acorr_x = np.asarray(acf(acceleration_x))
     tmp_acorr_y = np.asarray(acf(acceleration_y))
     tmp_acorr_z = np.asarray(acf(acceleration_z))
-    print("len ",len(acceleration_x))
-    print("len ",len(acceleration_y))
-    print("len ",len(acceleration_z))
+    print("length ",len(acceleration_x))
+    print("length ",len(acceleration_y))
+    print("length ",len(acceleration_z))
+
+#compute mean
+    tmp_amean_x = np.mean(acceleration_x)
+    tmp_amean_y = np.mean(acceleration_y)
+    tmp_amean_z = np.mean(acceleration_z)
+
+#free the array
     acceleration_x = np.delete(acceleration_x,range(len(acceleration_x)))
     acceleration_y = np.delete(acceleration_y,range(len(acceleration_y)))
     acceleration_z = np.delete(acceleration_z,range(len(acceleration_z)))
@@ -87,18 +95,26 @@ for j in xrange(0,22000,ntype):
         acorr_x = tmp_acorr_x
         acorr_y = tmp_acorr_y
         acorr_z = tmp_acorr_z
+
+        amean_x = tmp_amean_x
+        amean_y = tmp_amean_y
+        amean_z = tmp_amean_z
     else:
         for k in xrange(0,len(acorr_x)):
             acorr_x[k] += tmp_acorr_x[k]
             acorr_y[k] += tmp_acorr_y[k]
             acorr_z[k] += tmp_acorr_z[k]
 
+            amean_x += tmp_amean_x
+            amean_y += tmp_amean_y
+            amean_z += tmp_amean_z
+
     n=n+1.0
-    print("n =",n)
+    print("analyzed trajectories -> n = ",n)
 
     foutname = 'vcorr_'+sys.argv[4]+'.txt'
     fout = open(foutname,'w')
     for k in xrange(0,len(acorr_x)):
-        print(k*10,acorr_x[k]/acorr_x[0],acorr_y[k]/acorr_y[0],acorr_z[k]/acorr_z[0],file=fout)
+        print(k*10,acorr_x[k]/acorr_x[0],acorr_y[k]/acorr_y[0],acorr_z[k]/acorr_z[0],amean_x/n,amean_y/n,amean_z/n,file=fout)
     fout.close()
 
